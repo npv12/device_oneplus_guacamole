7467caf4a477f218391b29cc667c28b4de1bd365
Joey Huab <joey@evolution-x.org>
2020-12-08 23:36:05 +0900

    guacamole: Switch to Redfin's Dec 2020 fingerprint
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- xtended_guacamole.mk
+++ xtended_guacamole.mk
@@ -38,7 +38,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/2011052232:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7Pro \
     PRODUCT_NAME=OnePlus7Pro \
