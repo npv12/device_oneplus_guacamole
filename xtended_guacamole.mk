4e08e4a8838b2bce5388678f15c256374d2089fc
Pranav <npv12@iitbbs.ac.in>
2021-01-06 12:41:24 +0530

    Update fingerprint to redfin Jan 2021

--- xtended_guacamole.mk
+++ xtended_guacamole.mk
@@ -38,7 +38,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Build info
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus7Pro \
     PRODUCT_NAME=OnePlus7Pro \
